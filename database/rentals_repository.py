14123b9441d2d1253a27179f45cfe984d92c6c75
txg005 <marinovich.sl1@gmail.com>
2026-07-13 14:30:27 +0300

    added new method

--- database/rentals_repository.py
+++ database/rentals_repository.py
@@ -205,6 +205,19 @@ class RentalsRepository:
         ''', (start_date, end_date))
         return self.db.cursor.fetchall()
 
+    def get_bookings_by_car(self, start_date: str, end_date: str):
+        self.db.cursor.execute('''
+            SELECT c.brand, c.model, COUNT(r.id), SUM(r.total_cost)
+            FROM cars c
+            JOIN rentals r ON c.id = r.car_id
+                AND date(r.start_date) >= ? AND date(r.start_date) <= ?
+                AND r.status = 'забронировано'
+            GROUP BY c.id
+            HAVING COUNT(r.id) > 0
+            ORDER BY SUM(r.total_cost) DESC
+        ''', (start_date, end_date))
+        return self.db.cursor.fetchall()
+
     def get_bookings_by_month(self, start_date: str, end_date: str):
         self.db.cursor.execute('''
             SELECT strftime('%Y-%m', date(start_date)), COUNT(*), SUM(total_cost)
